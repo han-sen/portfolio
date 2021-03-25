e8237daadfe1b75cecc17bd8fda1448793382a94
Michael Hansen <michaelandhansen@gmail.com>
2021-03-25 00:20:20 -0400

    added back to top button

--- src/components/anim-link.js
+++ src/components/anim-link.js
@@ -3,9 +3,7 @@ import AniLink from "gatsby-plugin-transition-link/AniLink"
 
 export default function AnimatedLink(props) {
   const dirArr = ["left", "right", "down", "up"]
-  const colors = ["#254bc9", "#80e8c9"]
   const curArr = dirArr[Math.floor(Math.random() * 4)]
-  const curColor = colors[Math.floor(Math.random() * 2)]
   return (
     <AniLink cover direction={curArr} to={props.link} bg="#363636">
       {props.children}
